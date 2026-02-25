92deb2ec6d66c21aece67ecae1159f282e2ab96d
ethanan544-art <ethanan544@gmail.com>
2026-02-25 21:19:10 +0100

    Initial commit: Production Meteora sniper engine v3.0

--- sniper-engine.js
+++ sniper-engine.js
@@ -1,1 +1,4 @@
-<complete production code provided>
+import { Connection, PublicKey, VersionedTransaction, Keypair, LAMPORTS_PER_SOL, ComputeBudgetProgram, getAssociatedTokenAddress } from "@solana/web3.js";
+// Include all the wallet manager, risk analyzer, pool monitoring, Jupiter V6 swap execution, and Express API code here.
+
+// Code here...
